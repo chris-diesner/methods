cba70037f8ac846fdfda6f7630aa4929686d8a72
Christian Diesner <chr.diesner@mailbox.org>
2023-04-18 12:57:10 +0200

    KickStart

---
+++ src/main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class main {
+
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+        System.out.println("Gib die erste Zahl ein: ");
+        int input1 = scanner.nextInt();
+        System.out.println("Gib die zweite Zahl ein: ");
+        int input2 = scanner.nextInt();
+        System.out.println(doubleNumber(input1,input2));
+        System.out.println(printNumbers(input1,input2));
+        System.out.println("Ist eine der beiden Zahlen größer 100?:");
+        System.out.println(compareNumbers(input1,input2));
+    }
+
+    public static int doubleNumber (int num1, int num2){
+        return num1+num2;
+    }
+
+    public static String printNumbers(int num1, int num2){
+        String output = "Die Nummern " + num1 + " und " + num2 + " wurden addiert.";
+        return output;
+    }
+
+    public static boolean compareNumbers(int num1, int num2){
+        if (num1 > 100){
+            return true;
+        } else if(num2 > 100) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
